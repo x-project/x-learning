621c18c212219b8e95c1b232408261a551f1d4b9
alessandroRastelli <alessandro.rastelli89@gmail.com>
2015-10-30 16:07:38 +0100

    merge upload and transcder

---
+++ server/models/transcoder.js
@@ -0,0 +1,58 @@
+var aws = require('aws-sdk');
+
+module.exports = function (Transcoder) {
+
+  var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+  var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+  var S3_BUCKET = process.env.S3_BUCKET;
+  var S3_REGION = process.env.S3_REGION;
+  var PIPELINE_ID = process.env.PIPELINE_ID;
+  var PRESET_ID = process.env.PRESET_ID;
+
+  aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
+  aws.config.update({region: S3_REGION , signatureVersion: 'v4' });
+  
+  var elastictranscoder = new aws.ElasticTranscoder();
+
+  Transcoder.createJob = function (file_name,callback){
+    
+    var input_params = {
+      Input: { 
+        // Key: 'HD.mkv',
+        Key: file_name, 
+        FrameRate: 'auto', 
+        Resolution: 'auto', 
+        AspectRatio: 'auto', 
+        Interlaced: 'auto', 
+        Container: 'auto' 
+      }, 
+      PipelineId: PIPELINE_ID, // specifies output/input buckets in S3 
+      OutputKeyPrefix: 'videos/',
+      Output: { 
+        Key: file_name+'.m3u8', 
+        PresetId: PRESET_ID, // specifies the output video format
+        SegmentDuration: '5'
+      },
+
+    };
+    
+    elastictranscoder.createJob(input_params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+        callback(err);
+        return;
+      }else{
+        //console.log(data);
+        callback(null, data);
+      }
+    });
+  };
+  
+  Transcoder.remoteMethod('createJob', {
+    http: { verb: 'put' },
+    accepts: [
+      {arg: 'file_name', type: 'string'}
+    ],
+    returns: {arg: 'dataId', type: 'string'}
+  });
+};
