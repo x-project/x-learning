c36879e82a6d7e018656f0c4c18f2943974ea00f
alessandroRastelli <alessandro.rastelli89@gmail.com>
2015-11-09 17:52:02 +0100

    insert session and create page insert lecture and course

--- server/models/transcoder.js
+++ server/models/transcoder.js
@@ -1,9 +1,15 @@
 var aws = require('aws-sdk');
 
 module.exports = function (Transcoder) {
-
+  var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+  var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+  var S3_BUCKET = process.env.S3_BUCKET;
+  var S3_REGION = process.env.S3_REGION;
   var PIPELINE_ID = process.env.PIPELINE_ID;
   var PRESET_ID = process.env.PRESET_ID;
+
+  aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
+  aws.config.update({region: S3_REGION , signatureVersion: 'v4' });
   
   var elastictranscoder = new aws.ElasticTranscoder();
 
@@ -19,7 +25,7 @@ module.exports = function (Transcoder) {
         Container: 'auto' 
       }, 
       PipelineId: PIPELINE_ID, // specifies output/input buckets in S3 
-      OutputKeyPrefix: 'videos/',
+      OutputKeyPrefix: 'courses/courses_id/videos',
       Output: { 
         Key: file_name+'.m3u8', 
         PresetId: PRESET_ID, // specifies the output video format
