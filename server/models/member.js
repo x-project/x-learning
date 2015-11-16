24ee0023bc9c37234e719c5709e7f420f0e15526
alessandroRastelli <alessandro.rastelli89@gmail.com>
2015-11-16 13:07:54 +0100

    email change completed

--- server/models/member.js
+++ server/models/member.js
@@ -1,3 +1,116 @@
+var loopback = require('loopback');
+
 module.exports = function (Member) {
-  
+
+  function getCurrentUserId() {
+    var ctx = loopback.getCurrentContext();
+    var accessToken = ctx && ctx.get('accessToken');
+    var userId = accessToken && accessToken.userId;
+    return userId;
+  }
+
+  Member.change_email = function (confirm, email, password, callback) {
+    if (confirm !== email) {
+      cb(new Error('email not confirmed'), null);
+      return;
+    }
+
+    var user;
+    var user_id = getCurrentUserId();
+
+    var on_find_user = function (err, res) {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      user = res;
+      user.hasPassword(password, on_match_password);
+    }
+
+    var on_match_password = function (err, match) {
+      if (!match) {
+        callback(new Error('invalid password'), null);
+        return;
+      }
+      user.updateAttribute('email', confirm, on_update_email);
+    }
+
+    var on_update_email = function (err, user) {
+
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      callback(null, true);
+    };
+
+    Member.findById(user_id, on_find_user);
+  };
+
+  Member.remoteMethod('change_email', {
+    http: { path: '/change_email', verb: 'post' },
+    accepts: [
+      { arg: 'email', type: 'string' },
+      { arg: 'confirm', type: 'string' },
+      { arg: 'password', type: 'string' }
+    ],
+    returns: { arg: 'changed', type: 'boolean' }
+  });
+
+  Member.change_password = function (new_password, confirm_password, password, callback) {
+    if (new_password !== confirm_password) {
+      callback(new Error('password not confirmed'), null);
+      return;
+    }
+
+    var user;
+    var user_id = getCurrentUserId();
+
+    var on_find_user = function (err, instance) {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      user = instance;
+      user.hasPassword(password, on_match_password);
+    };
+
+    var on_match_password = function (err, match) {
+      if (!match) {
+        callback(new Error('invalid password'), null);
+        return;
+      }
+      user.updateAttribute('password', new_password, on_update_password);
+    };
+
+    var on_update_password = function (err, user) {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      callback(null, true);
+    };
+
+    Member.findById(user_id, on_find_user);
+  };
+
+  Member.remoteMethod('change_password', {
+    http: { path: '/change_password', verb: 'post' },
+    accepts: [
+      { arg: 'new_password', type: 'string' },
+      { arg: 'confirm_password', type: 'string' },
+      { arg: 'password', type: 'string' }
+    ],
+    returns: { arg: 'changed', type: 'boolean' }
+  });
+
+
+
+  Member.on('resetPasswordRequest', function (info) {
+    // console.log(info.user); // the requested user
+    // console.log(info.email); // the email of the requested user
+    // console.log(info.accessToken); // the temp access token to allow password reset
+    // TODO: send email to user
+  });
+
 };
