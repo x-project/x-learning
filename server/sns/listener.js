95fffb1ab29aab0ee39b93138b98610e0c395889
alessandroRastelli <alessandro.rastelli89@gmail.com>
2015-12-03 14:07:02 +0100

    add remove file s3 after elastic transcoder

--- server/sns/listener.js
+++ server/sns/listener.js
@@ -9,6 +9,7 @@ module.exports = function (sns) {
   aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
   aws.config.update({region: S3_REGION , signatureVersion: 'v4' });
 
+  var s3 = new aws.S3();
   var sqs = new aws.SQS();
   var queueUrl = "https://sqs.us-west-2.amazonaws.com/090870039614/x-learning-queue ";
 
@@ -19,32 +20,49 @@ module.exports = function (sns) {
   sqs.receiveMessage(receiveMessageParams, receiveMessageCallback);
 
   function receiveMessageCallback(err, data) {
-    console.log("received message");
-    console.log(data);
-
     if (data.Messages && data.Messages.length > 0) {
-
-      console.log("do something with the message here...");
-      console.log(data.Messages[0].MessageId)
-      console.log(data)
+      var message = JSON.parse(data.Messages[0].Body);
+      var key = JSON.parse(message.Message).input.key;
+      
       // Delete the message when we've successfully processed it
       var deleteMessageParams = {
         QueueUrl: queueUrl,
         ReceiptHandle: data.Messages[0].ReceiptHandle
       };
 
-      // sqs.deleteMessage(deleteMessageParams, deleteMessageCallback);
+      var s3_params = {
+        Bucket: S3_BUCKET,
+        Delete: { 
+          Objects: [ 
+            {
+              Key: key
+            }
+          ]
+        }
+      };
+      s3.deleteObjects(s3_params,deleteObjCallback)
+      sqs.deleteMessage(deleteMessageParams, deleteMessageCallback);
     }
     run_handler();
   }
 
   function deleteMessageCallback(err, data) {
-    console.log("deleted message");
+    console.log("deleted message SNS");
     console.log(data);
   }
 
   function run_handler () {
       sqs.receiveMessage(receiveMessageParams, receiveMessageCallback);
   };
+
+  function deleteObjCallback(err,data){
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      return;
+    }else{
+      console.log("deleted message S3");
+      console.log(data)
+    }
+  }
 
 }
