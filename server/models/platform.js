42a9fbe62e5c1ae65b7107b7b382f2be1b1bcc1a
alessandroRastelli <alessandro.rastelli89@gmail.com>
2016-02-26 17:23:35 +0100

    add platform

---
+++ server/models/platform.js
@@ -0,0 +1,7 @@
+module.exports = function (Platform) {
+
+  var email_pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  Platform.validatesFormatOf('email', {with: email_pattern, message: 'Must provide a valid email'});
+
+};
